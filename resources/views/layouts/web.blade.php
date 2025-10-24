e9eea35cd6e38dcf3769a50ec3134c00314e78c9
edipurwanto1988 <edipurwanto88@gmail.com>
2025-10-24 22:55:03 +0700

    tes

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Favicon -->
+        <!-- Faviconn -->
         <link rel="icon" type="image/svg+xml" href="{{ asset('images/biochamb_logo.svg') }}">
 
         <title>{{ $title ?? setting('company_name', config('app.name', 'Megah Persada Nusantara')) }}</title>
